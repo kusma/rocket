26ddc3fb81cd1f9978c602fd09392b7dbec6c147
Erik Faye-Lund <kusmabite@gmail.com>
2016-01-26 23:32:37 +0100

    editor: allow changing tabs with keyboard shortcuts

--- editor/trackview.cpp
+++ editor/trackview.cpp
@@ -853,7 +853,11 @@ void TrackView::keyPressEvent(QKeyEvent *event)
 	if (lineEdit->isHidden()) {
 		switch (event->key()) {
 		case Qt::Key_Backtab:
-			ctrlDown = false;
+			if (ctrlDown) {
+				// the main window will handle this
+				event->ignore();
+				return;
+			}
 			selecting = false;
 			// FALLTHROUGH
 		case Qt::Key_Left:
@@ -870,7 +874,11 @@ void TrackView::keyPressEvent(QKeyEvent *event)
 			return;
 
 		case Qt::Key_Tab:
-			ctrlDown = false;
+			if (ctrlDown) {
+				// the main window will handle this
+				event->ignore();
+				return;
+			}
 			selecting = false;
 			// FALLTHROUGH
 		case Qt::Key_Right:
@@ -941,7 +949,9 @@ void TrackView::keyPressEvent(QKeyEvent *event)
 	}
 
 	switch (event->key()) {
-	case Qt::Key_Delete: editClear(); return;
+	case Qt::Key_Delete:
+		editClear();
+		return;
 
 	case Qt::Key_Cancel:
 	case Qt::Key_Escape:
@@ -962,7 +972,7 @@ void TrackView::keyPressEvent(QKeyEvent *event)
 		return;
 	}
 
-	if (!readOnly && lineEdit->isHidden() && event->text().length() && doc->getTrackCount()) {
+	if (!readOnly && !ctrlDown && lineEdit->isHidden() && event->text().length() && doc->getTrackCount()) {
 		// no line-edit, check if input matches a double
 		QString str = event->text();
 		int pos = 0;
