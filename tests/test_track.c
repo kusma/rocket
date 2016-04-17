d15ddcb8002b65355395190c8bb730adcdac002d
Erik Faye-Lund <kusmabite@gmail.com>
2016-04-17 20:38:31 +0200

    test all key types

--- tests/test_track.c
+++ tests/test_track.c
@@ -72,11 +72,77 @@ int test_sync_get_val_linear()
 
 	struct sync_track *t = make_track("linear", keys, ARRAY_SIZE(keys));
 
+	const float w_q1 = 0.25f;
+	const float w_q2 = 0.5f;
+	const float w_q3 = 0.75f;
+
 	ASSERT_FLOAT_EQUAL(sync_get_val(t,   0.0f), 10.0f, 1e-5);
 	ASSERT_FLOAT_EQUAL(sync_get_val(t, 100.0f), 10.0f, 1e-5);
-	ASSERT_FLOAT_EQUAL(sync_get_val(t, 105.0f), 15.0f, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 102.5f), 10.0f + 10 * w_q1, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 105.0f), 10.0f + 10 * w_q2, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 107.5f), 10.0f + 10 * w_q3, 1e-5);
 	ASSERT_FLOAT_EQUAL(sync_get_val(t, 110.0f), 20.0f, 1e-5);
-	ASSERT_FLOAT_EQUAL(sync_get_val(t, 115.0f), 22.5f, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 112.5f), 20.0f + 5 * w_q1, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 115.0f), 20.0f + 5 * w_q2, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 117.5f), 20.0f + 5 * w_q3, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 120.0f), 25.0f, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 150.0f), 25.0f, 1e-5);
+
+	return 0;
+}
+
+int test_sync_get_val_smooth()
+{
+	struct track_key keys[] = {
+		{ 100, 10.0f, KEY_SMOOTH },
+		{ 110, 20.0f, KEY_SMOOTH },
+		{ 120, 25.0f, KEY_SMOOTH }
+	};
+
+	struct sync_track *t = make_track("linear", keys, ARRAY_SIZE(keys));
+
+	const float w_q1 = 0.15625f;
+	const float w_q2 = 0.5f;
+	const float w_q3 = 1.0f - w_q1;
+
+	ASSERT_FLOAT_EQUAL(sync_get_val(t,   0.0f), 10.0f, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 100.0f), 10.0f, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 102.5f), 10.0f + 10 * w_q1, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 105.0f), 10.0f + 10 * w_q2, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 107.5f), 10.0f + 10 * w_q3, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 110.0f), 20.0f, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 112.5f), 20.0f + 5 * w_q1, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 115.0f), 20.0f + 5 * w_q2, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 117.5f), 20.0f + 5 * w_q3, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 120.0f), 25.0f, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 150.0f), 25.0f, 1e-5);
+
+	return 0;
+}
+
+int test_sync_get_val_ramp()
+{
+	struct track_key keys[] = {
+		{ 100, 10.0f, KEY_RAMP },
+		{ 110, 20.0f, KEY_RAMP },
+		{ 120, 25.0f, KEY_RAMP }
+	};
+
+	struct sync_track *t = make_track("linear", keys, ARRAY_SIZE(keys));
+
+	const float w_q1 = 0.25f * 0.25f;
+	const float w_q2 = 0.5f * 0.5f;
+	const float w_q3 = 0.75f * 0.75f;
+
+	ASSERT_FLOAT_EQUAL(sync_get_val(t,   0.0f), 10.0f, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 100.0f), 10.0f, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 102.5f), 10.0f + 10 * w_q1, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 105.0f), 10.0f + 10 * w_q2, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 107.5f), 10.0f + 10 * w_q3, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 110.0f), 20.0f, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 112.5f), 20.0f + 5 * w_q1, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 115.0f), 20.0f + 5 * w_q2, 1e-5);
+	ASSERT_FLOAT_EQUAL(sync_get_val(t, 117.5f), 20.0f + 5 * w_q3, 1e-5);
 	ASSERT_FLOAT_EQUAL(sync_get_val(t, 120.0f), 25.0f, 1e-5);
 	ASSERT_FLOAT_EQUAL(sync_get_val(t, 150.0f), 25.0f, 1e-5);
 
@@ -88,6 +154,8 @@ int main(void)
 	TEST_RUN(test_track_basics);
 	TEST_RUN(test_sync_get_val_step);
 	TEST_RUN(test_sync_get_val_linear);
+	TEST_RUN(test_sync_get_val_smooth);
+	TEST_RUN(test_sync_get_val_ramp);
 
 	return test_report();
 }
